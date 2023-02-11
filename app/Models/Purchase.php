cd77a8c2bec7d2afbf0a23b3730a7bc443381fbb
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2023-02-11 11:03:02 +0200

    handle deleted customer for purchase so that we keep purchase of deleted customers

--- app/Models/Purchase.php
+++ app/Models/Purchase.php
@@ -34,7 +34,7 @@ class Purchase extends Model
      */
     public function user(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class)->withTrashed();
     }
 
     public function products()
